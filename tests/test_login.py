9eca3b014a4c26c86283b7a832e8ecb9b5bfc0a7
Pranab Ghosh <pranabit6@gmail.com>
2026-06-28 12:36:13 +0530

    added files

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,12 @@
+import pytest
 from pages.LoginPage import LoginPage
 from config.framework_config import config
 
-
+@pytest.mark.smoke
 def test_login(page):
     login = LoginPage(page)
 
     login.open(config.base_url)
 
     login.login("Admin", "admin123")
-
-    return page
+    login.verify_login()
